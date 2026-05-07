ba50bce6afbe1feaeecaa1c15109e3ea6ccc21cf
Aldi Dewangga <>
2026-05-07 09:53:34 +0700

    refactor: remove unused form-builder and groups modules

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -2,14 +2,12 @@ import { configureStore } from "@reduxjs/toolkit";
 import authReducer from "@/pages/auth/store/reducer";
 import usersReducer from "@/pages/users/store/reducer";
 import rolesReducer from "@/pages/roles/store/reducer";
-import groupsReducer from "@/pages/groups/store/reducer";
 
 export const store = configureStore({
   reducer: {
     auth: authReducer,
     users: usersReducer,
     roles: rolesReducer,
-    groups: groupsReducer,
   },
 });
 
